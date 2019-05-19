ff529c686891c885b6aa8a08e7f75b05fefb6019
Kasididh <buwinkasididh@hotmail.com>
2019-05-20 01:23:01 +0700

    580611004
    HW5

--- client/app/following/following.js
+++ client/app/following/following.js
@@ -13,5 +13,14 @@ angular.module('followingList', ['ngRoute'])
       $http.get('http://localhost:8080/twitterapi/following/', requestOptions).then(function (response) {
         self.followings = response.data;
       });
+        self.sendFollow = function sendFollow(follow_name) {
+            const data = "follow_name=" + encodeURIComponent(follow_name);
+            $http.post('http://localhost:8080/twitterapi/following/', data, requestOptions);
+        }
+        self.sendUnFollow = function sendUnFollow(follow_name) {
+            $http.defaults.headers.delete = { 'X-session': $rootScope.x_session };
+            const data = "follow_name=" + encodeURIComponent(follow_name);
+            $http.delete('http://localhost:8080/twitterapi/following/?' + data);
+        }
     }]
 });
